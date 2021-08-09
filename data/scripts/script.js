61dda08cf8e26aab65ef45ca798b05255038b681
Roman <Rkrivoschekov@lanit.ru>
2021-08-09 23:48:10 +0500

    09.08 23:48 sleep

--- data/scripts/script.js
+++ data/scripts/script.js
@@ -1,2 +1,22 @@
-let m
-alert(m);
+//let m
+//m = prompt('Введите ваше имя', 'Графоуни');
+let login = prompt('Введите ваш Логин', 'Admin');
+let pas;
+if (login === 'Admin') {
+    pas = prompt('Ваш пароль пожалуйста', '');
+    if (pas === 'Я главный') {
+        alert('Добро пожаловать Государь!');
+    } else {
+        if (pas === null) {
+            alert('Отмена');
+        } else {
+            alert('Не верный пароль');
+        }
+    }
+} else {
+    if (login === null) {
+        alert('Отмена');
+    } else {
+        'Я вас не знаю'
+    }
+}
